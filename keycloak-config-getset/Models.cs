f0753a0915bfc042b707809d0c09a2b299679015
Sofyan Ardianto <sofyanard@gmail.com>
2025-01-16 16:46:28 +0700

    add some code :frog:

---
+++ keycloak-config-getset/Models.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace keycloak_config_getset
+{
+    public class RealmAttribute
+    {
+        [JsonPropertyName("cibaBackchannelTokenDeliveryMode")]
+        public string? CibaBackchannelTokenDeliveryMode { get; set; }
+
+        [JsonPropertyName("cibaExpiresIn")]
+        public string? CibaExpiresIn { get; set; }
+
+        [JsonPropertyName("cibaAuthRequestedUserHint")]
+        public string? CibaAuthRequestedUserHint { get; set; }
+
+        [Description("OAuth 2.0 Device Code Lifespan")]
+        [JsonPropertyName("oauth2DeviceCodeLifespan")]
+        public string? Oauth2DeviceCodeLifespan { get; set; }
+
+        [Description("OAuth 2.0 Device Polling Interval")]
+        [JsonPropertyName("oauth2DevicePollingInterval")]
+        public string? Oauth2DevicePollingInterval { get; set; }
+
+        [JsonPropertyName("clientOfflineSessionMaxLifespan")]
+        public string? ClientOfflineSessionMaxLifespan { get; set; }
+
+        [JsonPropertyName("clientSessionIdleTimeout")]
+        public string? ClientSessionIdleTimeout { get; set; }
+
+        [JsonPropertyName("userProfileEnabled")]
+        public string? UserProfileEnabled { get; set; }
+
+        [Description("Lifetime of the Request URI for Pushed Authorization Request")]
+        [JsonPropertyName("parRequestUriLifespan")]
+        public string? ParRequestUriLifespan { get; set; }
+
+        [JsonPropertyName("clientSessionMaxLifespan")]
+        public string? ClientSessionMaxLifespan { get; set; }
+
+        [JsonPropertyName("clientOfflineSessionIdleTimeout")]
+        public string? ClientOfflineSessionIdleTimeout { get; set; }
+
+        [JsonPropertyName("cibaInterval")]
+        public string? CibaInterval { get; set; }
+    }
+
+    public class RealmToken
+    {
+        [Description("Default Signature Algorithm")]
+        [JsonPropertyName("defaultSignatureAlgorithm")]
+        public string? DefaultSignatureAlgorithm { get; set; }
+
+        [Description("Revoke Refresh Token")]
+        [JsonPropertyName("revokeRefreshToken")]
+        public bool RevokeRefreshToken { get; set; }
+
+        [Description("SSO Session Idle")]
+        [JsonPropertyName("ssoSessionIdleTimeout")]
+        public int SsoSessionIdleTimeout { get; set; }
+
+        [Description("SSO Session Max")]
+        [JsonPropertyName("ssoSessionMaxLifespan")]
+        public int SsoSessionMaxLifespan { get; set; }
+
+        [Description("SSO Session Idle Remember Me")]
+        [JsonPropertyName("ssoSessionIdleTimeoutRememberMe")]
+        public int SsoSessionIdleTimeoutRememberMe { get; set; }
+
+        [Description("SSO Session Max Remember Me")]
+        [JsonPropertyName("ssoSessionMaxLifespanRememberMe")]
+        public int SsoSessionMaxLifespanRememberMe { get; set; }
+
+        [Description("Offline Session Idle")]
+        [JsonPropertyName("offlineSessionIdleTimeout")]
+        public int OfflineSessionIdleTimeout { get; set; }
+
+        [Description("Offline Session Max Limited")]
+        [JsonPropertyName("offlineSessionMaxLifespanEnabled")]
+        public bool OfflineSessionMaxLifespanEnabled { get; set; }
+
+        [Description("Client Session Idle")]
+        [JsonPropertyName("clientSessionIdleTimeout")]
+        public int ClientSessionIdleTimeout { get; set; }
+
+        [Description("Client Session Max")]
+        [JsonPropertyName("clientSessionMaxLifespan")]
+        public int ClientSessionMaxLifespan { get; set; }
+
+        [Description("Access Token Lifespan")]
+        [JsonPropertyName("accessTokenLifespan")]
+        public int AccessTokenLifespan { get; set; }
+
+        [Description("Access Token Lifespan For Implicit Flow")]
+        [JsonPropertyName("accessTokenLifespanForImplicitFlow")]
+        public int AccessTokenLifespanForImplicitFlow { get; set; }
+
+        [Description("Client login timeout")]
+        [JsonPropertyName("accessCodeLifespanLogin")]
+        public int AccessCodeLifespanLogin { get; set; }
+
+        [Description("Login action timeout")]
+        [JsonPropertyName("accessCodeLifespanUserAction")]
+        public int AccessCodeLifespanUserAction { get; set; }
+
+        [Description("User-Initiated Action Lifespan")]
+        [JsonPropertyName("actionTokenGeneratedByUserLifespan")]
+        public int ActionTokenGeneratedByUserLifespan { get; set; }
+
+        [Description("Default Admin-Initiated Action Lifespan")]
+        [JsonPropertyName("actionTokenGeneratedByAdminLifespan")]
+        public int ActionTokenGeneratedByAdminLifespan { get; set; }
+
+        [JsonPropertyName("attributes")]
+        public RealmAttribute? TokenAttribute { get; set; }
+    }
+}
--- keycloak-config-getset/Program.cs
+++ keycloak-config-getset/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddConsole()
+        .SetMinimumLevel(LogLevel.Information);
+});
+var logger = loggerFactory.CreateLogger<Program>();
+
+var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("keycloak-config-getset/appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+var srcHost = configuration["Source:Host"];
+var srcRealm = configuration["Source:Realm"];
+var srcClientId = configuration["Source:ClientId"];
+var srcClientSecret = configuration["Source:ClientSecret"];
+
+var dstHost = configuration["Destination:Host"];
+var dstRealm = configuration["Destination:Realm"];
+var dstClientId = configuration["Destination:ClientId"];
+var dstClientSecret = configuration["Destination:ClientSecret"];
+
+logger.LogInformation("{0} - {1} - {2} - {3}", srcHost, srcRealm, srcClientId, srcClientSecret);
+logger.LogInformation("{0} - {1} - {2} - {3}", dstHost, dstRealm, dstClientId, dstClientSecret);
+
 Console.WriteLine("Hello, World!");
